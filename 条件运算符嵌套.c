b6550431077f3985fe080e75aca26fb431dc6cb1
nijiushi <547017470@qq.com>
2019-05-09 18:31:56 +0800

    提交代码

--- 条件运算符嵌套.c
+++ 条件运算符嵌套.c
@@ -3,22 +3,11 @@
 #include<stdlib.h>
 int main()
 {
-	char c;
-	int letters = 0, spaces = 0, digits = 0, others = 0;
-	printf("请输入一些字母：\n");
-	while ((c = getchar()) != '\n')
-	{
-		if ((c >= 'a'&&c <= 'z') || (c >= 'A'&&c <= 'Z'))
-			letters++;
-		else if (c >= '0'&&c <= '9')
-			digits++;
-		else if (c == ' ')
-			spaces++;
-		else
-			others++;
-	}
-	printf("字母=%d,数字=%d,空格=%d,其他=%d\n", letters, digits, spaces, others);
-
+	int score = 0;
+	char grade;
+	scanf("%d", &score);
+	grade = (score >= 90) ? 'A ' : ((score >= 60) ? ' B ' : 'C');
+	printf("%c", grade);
 	system("pause");
 	return 0;
 }
